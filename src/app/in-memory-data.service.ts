d196e2fd4656c44f48bff3324114231c192b51e9
marispulk@gmail.com <55805148+marispulk@users.noreply.github.com>
2020-08-28 10:48:31 +0300

    Updated styling, added flexbox, updated DB rows *Added d-flex and other Bootstrap classes *Removed unnecessary rows from CSS *Simulated different length rows in DB to test flexbox

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,16 +9,15 @@ export class InMemoryDataService implements InMemoryDataService {
 
   createDb() { 
     const chatmessages = [
-      {id: 1, username: 'Admin', content: 'Message 1', date: '25.08.2020 12:20:00'},
-      {id: 2, username: 'Admin', content: 'Message 2', date: '25.08.2020 12:21:00'},
-      {id: 3, username: 'Guest', content: 'Message 3', date: '25.08.2020 12:22:00'},
-      {id: 4, username: 'Admin', content: 'Message 4', date: '25.08.2020 12:23:00'},
-      {id: 5, username: 'Guest', content: 'Message 5', date: '25.08.2020 12:24:00'},
-      {id: 6, username: 'Admin', content: 'Message 6', date: '25.08.2020 12:25:00'}
+      {id: 1, username: 'Admin', content: 'Tere!', date: '25.08.2020 12:20:00'},
+      {id: 2, username: 'Admin', content: 'Tere Teilegi! ', date: '25.08.2020 12:21:00'},
+      {id: 3, username: 'Guest', content: 'Mis kell on tänane koosolek?', date: '25.08.2020 12:22:00'},
+      {id: 4, username: 'Admin', content: 'Maris ütles, et 10.00', date: '25.08.2020 12:23:00'},
+      {id: 5, username: 'Guest', content: 'Selge, aitäh', date: '25.08.2020 12:24:00'},
+      {id: 6, username: 'Admin', content: 'Pole tänu väärt!', date: '25.08.2020 12:25:00'}
       ];
       return {chatmessages};
   }
-
   // Overrides the genID method to ensure that a chatmessage always has a id.
   // If the chatmessages array is empty the method below returns the initial number (1).
   // If the heroes array is not empty, the method below returns the highest hero id + 1
