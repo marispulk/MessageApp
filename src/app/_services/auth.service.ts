2610a9ab63ab4658d4c6a62561ced699069894ee
Maris Pulk <marispulk@gmail.com>
2020-09-27 13:12:16 +0300

    Hotfix: User registration
    * Added extra functionality to credential sign-up method
    * Extra user data is now saved into Firebase users collection, under correct document.

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
-import { AngularFirestore} from "@angular/fire/firestore";
+import { AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
 import { Router } from '@angular/router';
 
 import { Observable, of } from 'rxjs';
@@ -15,6 +15,7 @@ export class AuthService {
 
   user$: Observable<User>;
   uid$: string;
+  userCollection: AngularFirestoreCollection<User>;
 
   async credentialSignIn(email: string, password: string) {
     const credential = await this.afauth.signInWithEmailAndPassword(email, password);
@@ -22,35 +23,39 @@ export class AuthService {
     // return this.updateUserData(credential.user);
   }
 
-    async credentialSignOut() {
-      const signout = await this.afauth.signOut();
-      this.router.navigate(['login']);
-    }
+  credentialSignUp(email: string, password: string) {
+    this.afauth.createUserWithEmailAndPassword(email, password)
+      .then(userdata => {
+        return this.database.collection('users').doc(userdata.user.uid).set({
+          uid: userdata.user.uid,
+          email: userdata.user.email,
+          //Set default display picture
+          displayPicture: 'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png',
+          //Default display name(all before @)
+          displayName: userdata.user.email.substring(0, userdata.user.email.indexOf('@'))
+        })
 
-    currentUser() {
-      return this.uid$;
-    }
-  // private updateUserData({ uid, email }:User) {
-  //   const userRef: AngularFirestoreDocument<User> = this.afstore.doc(`users/${uid}`);
+      });
+  }
 
-  //   const data = {
-  //     uid: uid,
-  //     email: email,
-  //     displayName: displayName
-  //   }
+  async credentialSignOut() {
+    const signout = await this.afauth.signOut();
+    this.router.navigate(['login']);
+  }
 
-  //   return userRef.set(data, { merge: true });
-  // }
+  currentUser() {
+    return this.uid$;
+  }
 
   constructor(
     private afauth: AngularFireAuth,
-    private afstore: AngularFirestore,
+    private database: AngularFirestore,
     private router: Router
   ) {
     this.user$ = this.afauth.authState.pipe(
       switchMap(user => {
         if(user) {
-          return this.afstore.doc<User>(`users/${user.uid}`).valueChanges();
+          return this.database.doc<User>(`users/${user.uid}`).valueChanges();
         } else {
           return of(null);
         }
