76d981b85d41a230d08e7f462ed5370f6fcabe55
Maris Pulk <marispulk@gmail.com>
2020-10-01 19:57:51 +0300

    Spinners, scroll-to bottom, Welcome component

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -11,11 +11,16 @@ import { ChatService } from "../chat.service";
 export class ChatsComponent implements OnInit {
 
   selectedChat: Chat;
+  loading: boolean = false;
   chats: Chat[];
 
   getChats(): void {
+    this.loading = true;
     this.chatService.getChats()
-    .subscribe(chats => this.chats = chats)
+    .subscribe(chats =>  {
+      this.loading = false;
+      this.chats = chats;
+    })
   }
 
   addChats(chatName: string, chatPicture: string): void {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDBvnLWYWPf9dO20JkCbclfZhrOptE4w7g",
+    authDomain: "message-app-ff56f.firebaseapp.com",
+    databaseURL: "https://message-app-ff56f.firebaseio.com",
+    projectId: "message-app-ff56f",
+    storageBucket: "message-app-ff56f.appspot.com",
+    messagingSenderId: "338385062636"
+  }
 };
